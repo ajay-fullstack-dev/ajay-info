95c20a68572a14f58bca12054ca29a5a2bbede74
Ajay Modi <amajaymodi4@gmail.com>
2026-05-19 15:55:29 +0530

    Add Resume download features

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+  RESUME_PATH = Rails.root.join("public/downloads/ajay_modi_resume.pdf")
+  RESUME_FILENAME = "Ajay_Modi_Resume.pdf"
+
   def index
+  end
+
+  def resume
+    unless RESUME_PATH.exist?
+      head :not_found
+      return
+    end
+
+    send_file RESUME_PATH,
+              filename: RESUME_FILENAME,
+              type: "application/pdf",
+              disposition: "attachment"
   end
 end
